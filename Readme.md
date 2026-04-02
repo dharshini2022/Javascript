24231458885efe769c2296ce867bb987815cfd25
Dharshini <dharshini.k2022cce@sece.ac.in>
2026-04-02 22:10:21 +0530

    TASK 2 - Simple Calculator

--- Readme.md
+++ Readme.md
@@ -1,7 +1,13 @@
 **TASK 1:**
 **Assignment:** Interactive TODO List APPLICATION
 **TECH STACK:**
-* state management and local storage
-* html element management with js (create,update and delete)
-* implemented toggle functions for TODO list task completion status
+* State management and local storage
+* Html element management with js (create,update and delete)
+* Implemented toggle functions for TODO list task completion status
 * DOM operations 
+
+**TASK 2:**
+**Assignment:** Simple Calculator
+**TECH STACK:**
+* Dynamic display based on user interaction
+* Input captured from HTML elements, using Javascript
---
+++ task1-TODO_LIST/task2-Calculator/script.js
@@ -0,0 +1,35 @@
+const display = document.querySelector(".displayDiv");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const clear = document.getElementById("AC");
+const equalTo = document.getElementById("equalTo");
+
+let currentDisplay = "";
+
+numbers.forEach(button => {
+    button.addEventListener("click", () => handleClick(button));
+});
+
+operators.forEach(button => {
+    button.addEventListener("click", () => handleClick(button));
+});
+
+function handleClick(button){
+    currentDisplay += button.innerText;
+    display.innerText = currentDisplay;
+}
+
+equalTo.addEventListener("click", () => {
+    try{
+        currentDisplay = eval(currentDisplay).toString();
+        display.innerText = currentDisplay
+    }catch{
+        display.innerText = "Error";
+        currentDisplay = "";
+    }
+});
+
+clear.addEventListener("click",() => {
+    currentDisplay = "";
+    display.innerText="";
+});
