d827bb2558a10ec739629dbf2905bb5d8cf9e7db
Dharshini <dharshini.k2022cce@sece.ac.in>
2026-04-02 16:14:55 +0530

    TASK 1 - TODO List

---
+++ Readme.md
@@ -0,0 +1,7 @@
+**TASK 1:**
+**Assignment:** Interactive TODO List APPLICATION
+**TECH STACK:**
+* state management and local storage
+* html element management with js (create,update and delete)
+* implemented toggle functions for TODO list task completion status
+* DOM operations 
---
+++ task1-TODO_LIST/script.js
@@ -0,0 +1,58 @@
+const input = document.getElementById("addTaskInput");
+const addBtn = document.getElementById("addTaskBtn");
+const todoList = document.getElementById("todoList");
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function displayTasks() {   //called whenever the UI is updated
+    todoList.innerHTML = "";
+
+    tasks.forEach((task,index) => {
+        const list = document.createElement("li");
+        list.textContent = task.text;
+
+        if(task.completed){
+            list.classList.add("completed");
+        }
+
+        list.addEventListener("click", () => {
+            tasks[index].completed = !tasks[index].completed;
+            savetoLocalStorage();
+            displayTasks();
+        });
+
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "X";
+        deleteBtn.classList.add("deleteBtn")
+
+        deleteBtn.addEventListener("click", (e) => {
+            e.stopPropagation();
+            tasks.splice(index,1);
+            savetoLocalStorage();
+            displayTasks();
+        });
+
+        list.appendChild(deleteBtn);
+        todoList.appendChild(list);
+    });
+}
+
+addBtn.addEventListener("click", (e) => {
+        const text = input.value;
+
+        if(text === "") return;
+
+        tasks.push({text,completed: false});
+        input.value = "";
+
+        savetoLocalStorage();
+        displayTasks();
+})
+
+
+function savetoLocalStorage(){
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+displayTasks();
